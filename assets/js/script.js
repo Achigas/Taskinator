94e34b981389fff20351ce53b7baa331cbf279d2
AChigas <Achigas34@gmail.com>
2020-06-10 00:26:07 -0700

    Avoided default refresh by browser

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-buttonEl.addEventListener("click", function() {
+//create new task list item 
+var createTaskHandler = function (event) {
+
+  event.preventDefault();
+    
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
   listItemEl.textContent = "This is a new task.";
   tasksToDoEl.appendChild(listItemEl);
-});
+};
+
+//on button click or enter - create a task
+formEl.addEventListener("submit", createTaskHandler);
